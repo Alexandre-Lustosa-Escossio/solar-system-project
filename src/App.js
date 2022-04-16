1793621f42227ded3f089236f353e0e9d5c8147f
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 09:15:05 -0300

    feat: creates Missions component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Missions from './components/Missions'
 import SolarSystem from './components/SolarSystem'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <main>
       <Header />
       <SolarSystem />
+      <Missions />
     </main>
   )
 }
