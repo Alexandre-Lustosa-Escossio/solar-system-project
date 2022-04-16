f2e140760eeecd3ff0f38f9d0a83969b3ae32087
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 08:35:35 -0300

    feat:adds Title component propTypes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import propTypes from 'prop-types' 
 
 export default function Title({ headline }) {
   return (
@@ -6,4 +7,8 @@ export default function Title({ headline }) {
         <h2>{ headline }</h2>
     </div>
   )
+}
+
+Title.propTypes = {
+    headline: propTypes.string.isRequired
 }
