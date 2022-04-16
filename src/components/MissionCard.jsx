df7b26f1851601a0053cd2eca03067d330e73a44
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 09:25:52 -0300

    feat: creates MissionCard component

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+export default function MissionCard( props ) {
+    const {
+        name,
+        year,
+        country,
+        destination
+    } = props
+  return (
+    <div>
+        <p>{ name }</p>
+        <p>{ year }</p>
+        <p>{ country }</p>
+        <p>{ destination }</p>
+    </div>
+  )
+}
+
+MissionCard.propTypes = {
+    name: propTypes.string,
+    year:propTypes.string,
+    country:propTypes.string,
+    destination:propTypes.string,
+}.isRequired
