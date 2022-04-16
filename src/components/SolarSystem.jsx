4ccb933f7d3c041f82e337437c631796b3067f4f
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 09:12:27 -0300

    feat: maps planet data rendering each planet in a PlanetCard component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Title from './Title'
+import planets from '../data/planets'
+import PlanetCard from './PlanetCard'
 
 export default function SolarSystem() {
   return (
     <div>
         Solar System Component
         <Title headline={ 'Planets' } />
+        {planets.map(planet => 
+          <PlanetCard planetName={ planet.name } planetImage={ planet.image }/>
+        )}
     </div>
   )
 }
--- src/data/planets.js
+++ src/data/planets.js
@@ -1,21 +1,21 @@
-import MercurioImg from '../images/Mercurio.png';
+import MercuryImg from '../images/Mercury.png';
 import VenusImg from '../images/Venus.png';
-import TerraImg from '../images/Terra.png';
-import MarteImg from '../images/Marte.png';
+import EarthImg from '../images/Earth.png';
+import MarsImg from '../images/Mars.png';
 import JupiterImg from '../images/Jupiter.png';
-import SaturnoImg from '../images/Saturno.png';
-import Uranoimg from '../images/Urano.png';
-import Netunoimg from '../images/Netuno.png';
+import SaturnImg from '../images/Saturn.png';
+import Uranusimg from '../images/Uranus.png';
+import Neptuneimg from '../images/Neptune.png';
 
 const Planets = [
-  { name: 'Mercúrio', image: MercurioImg },
-  { name: 'Vênus', image: VenusImg },
-  { name: 'Terra', image: TerraImg },
-  { name: 'Marte', image: MarteImg },
+  { name: 'Mercury', image: MercuryImg },
+  { name: 'Venus', image: VenusImg },
+  { name: 'Earth', image: EarthImg },
+  { name: 'Mars', image: MarsImg },
   { name: 'Júpiter', image: JupiterImg },
-  { name: 'Saturno', image: SaturnoImg },
-  { name: 'Urano', image: Uranoimg },
-  { name: 'Netuno', image: Netunoimg },
+  { name: 'Saturn', image: SaturnImg },
+  { name: 'Uranus', image: Uranusimg },
+  { name: 'Neptune', image: Neptuneimg },
 ];
 
 export default Planets;
