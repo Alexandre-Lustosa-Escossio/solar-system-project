1b9af9c3fc3c8b138010e1e2f583c5505ba01ce1
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 08:28:48 -0300

    feat: creates Title component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Title from './Title'
 
 export default function SolarSystem() {
   return (
     <div>
         Solar System Component
+        <Title headline={ 'Planets' } />
     </div>
   )
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title({ headline }) {
+  return (
+    <div>
+        <h2>{ headline }</h2>
+    </div>
+  )
+}
