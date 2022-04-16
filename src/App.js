6edec77e3059ca621ff2ee00fdd6aa3419af3fa5
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 08:25:52 -0300

    feat: creates SolarSystem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import SolarSystem from './components/SolarSystem'
 
 export default function App() {
   return (
     <main>
       <Header />
+      <SolarSystem />
     </main>
   )
 }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SolarSystem() {
+  return (
+    <div>
+        Solar System Component
+    </div>
+  )
+}
