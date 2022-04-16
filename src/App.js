25e259b20476b90e58633a169ee09a68b4258328
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 20:38:13 -0300

    style: PlanetCard stylization

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './components/Header'
 import Missions from './components/Missions'
 import SolarSystem from './components/SolarSystem'
+import './App.css'
 
 export default function App() {
   return (
-    <main className='d-flex flex-column align-items-center'>
+    <main className='d-flex flex-column align-items-center'
+    >
       <Header />
       <SolarSystem />
       <Missions />
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import '../style/PlanetCard.css'
 
 export default function PlanetCard({ planetName, planetImage }) {
   return (
     <div data-testid='planet-card'
-    className='border'>
-        <p>{ planetName }</p>
-        <img className='class="d-block w-100"' 
+    className='card bg-secondary bg-gradient bg-opacity-10 m-2 justify-content-center
+    align-items-center'>
+        <p className=''>{ planetName }</p>
+        <img className='' 
         src={ planetImage } 
         alt='planet'/>
     </div>
