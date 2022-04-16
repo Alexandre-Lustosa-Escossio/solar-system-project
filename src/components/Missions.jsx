5be4982b942c524e02561e19b9dc8903ca86cb6d
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 09:54:30 -0300

    feat: maps each mission to a MissionCard

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Title from './Title'
+import missions from '../data/missions'
+import MissionCard from './MissionCard'
 
 export default function Missions() {
   return (
     <div>
         <Title headline='Missions'/>
+        {missions.map(mission => 
+            <MissionCard {...mission} /> )}
     </div>
   )
 }
