dab3873576b62a64002fb8f04aa9755af68acf97
Alexandre <lutosaalexandre@gmail.com>
2022-04-16 10:19:34 -0300

    fix: correction on Title component tests

--- src/tests/Title.test.js
+++ src/tests/Title.test.js
@@ -4,8 +4,8 @@ import Title from "../components/Title";
 
 describe('tests title component', () => {
     it('should have a h2 tag with the headline text', () => {
-        render(<Title />)
-        const heading = screen.getByRole('heading', { level:2 })
+        render(<Title headline="some headline"/>)
+        const heading = screen.getByRole('heading', { level:2, name:/some headline/i })
         expect(heading).toBeInTheDocument()
     });
  })
